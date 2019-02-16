491c6035ed2d59345d98968e910c77d49ff01d84
Dan Fox <dfox@palantir.com>
2019-02-16 12:59:09 +0000

    itertools cartesian product to solve day 1 part 2

--- src/main.rs
+++ src/main.rs
@@ -59,8 +59,11 @@ mod day1 {
 
 #[cfg(test)]
 mod day2 {
+    use itertools::Itertools;
+    use std::collections::HashSet;
     use std::fs;
     use std::iter::FromIterator;
+    use std::string::String;
 
     #[test]
     fn part1() {
@@ -70,6 +73,52 @@ mod day2 {
             .map(ChecksumEntry::from)
             .collect();
         assert_eq!(checksum, Checksum(5928));
+    }
+
+    fn find_strings_differing_by_1(input: Vec<&str>) -> HashSet<String> {
+        let mut results = HashSet::new();
+
+        for (left, right) in input.iter().cartesian_product(input.iter()) {
+            let (num, matches) = diff_strings(left, right);
+            if num == 1 {
+                results.insert(matches);
+            }
+        }
+
+        results
+    }
+
+    #[test]
+    fn part2() {
+        assert_eq!(
+            find_strings_differing_by_1(vec!("abcde", "fghij", "klmno", "pqrst", "fguij", "axcye", "wvxyz")),
+            HashSet::from_iter(vec!("fgij".to_owned()))
+        );
+
+        assert_eq!(
+            find_strings_differing_by_1(fs::read_to_string("day2.txt").unwrap().lines().collect()),
+            HashSet::from_iter(vec!("bqlporuexkwzyabnmgjqctvfs".to_owned()))
+        )
+    }
+
+    fn diff_strings(left: &str, right: &str) -> (usize, String) {
+        assert_eq!(left.len(), right.len());
+
+        let matching: Vec<u8> = left.as_bytes().iter().zip(right.as_bytes())
+            .filter(|(&left_byte, &right_byte)| left_byte == right_byte)
+            .map(|(&left_byte, _)| left_byte)
+            .collect();
+
+        let differing_chars = left.len() - matching.len();
+        let matching_str: &str = std::str::from_utf8(&matching).unwrap();
+
+        (differing_chars, matching_str.to_owned())
+    }
+
+    #[test]
+    fn count_differing_characters_samples() {
+        assert_eq!(diff_strings("fghij", "fguij"), (1, "fgij".to_owned()));
+        assert_eq!(diff_strings("abcde", "axcye"), (2, "ace".to_owned()));
     }
 
     #[derive(Debug, PartialEq)]
