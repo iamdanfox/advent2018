5e3b0f562fab84e1a0ad0e4fd53dfa0fe6b224f7
Dan Fox <dfox@palantir.com>
2019-02-16 11:59:44 +0000

    Correct answer for day2 part1

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,6 @@ mod day1 {
         assert_eq!(find_first_repeat(vec![-6, 3, 8, 5, -6]), Some(5));
         assert_eq!(find_first_repeat(vec![7, 7, -2, -7, -4]), Some(14));
 
-
         assert_eq!(find_first_repeat(input()), Some(549));
     }
 
@@ -29,11 +28,14 @@ mod day1 {
         let result = std::fs::read_to_string("day1.txt").unwrap();
         let lines = result.lines();
 
-        lines.map(|line| line.parse().expect(&format!("trying to parse '{}'", line))).collect()
+        lines
+            .map(|line| line.parse().expect(&format!("trying to parse '{}'", line)))
+            .collect()
     }
 
     fn accumulate_frequencies(input: Vec<i32>) -> Vec<i32> {
-        input.iter()
+        input
+            .iter()
             .scan(0, |freq, change| {
                 *freq = *freq + change;
                 return Some(*freq);
@@ -57,12 +59,14 @@ mod day1 {
 
 #[cfg(test)]
 mod day2 {
+    use std::fs;
+
     #[derive(Debug, PartialEq)]
-    struct Checksum(u8);
+    struct Checksum(u64);
 
     fn checksum(entries: &[ChecksumEntry]) -> Checksum {
-        let mut twos: u8 = 0;
-        let mut threes: u8 = 0;
+        let mut twos: u64 = 0;
+        let mut threes: u64 = 0;
         for &entry in entries {
             twos += if entry.exactly_2_of_any { 1 } else { 0 };
             threes += if entry.exactly_3_of_any { 1 } else { 0 };
@@ -89,30 +93,63 @@ mod day2 {
             let mut three = false;
 
             for &count in char_counts.iter() {
-                if two && three {
-                    break;
-                }
                 match count {
                     2 => two = true,
                     3 => three = true,
                     _ => {}
                 }
             }
-            ChecksumEntry { exactly_2_of_any: two, exactly_3_of_any: three }
+            ChecksumEntry {
+                exactly_2_of_any: two,
+                exactly_3_of_any: three,
+            }
         }
     }
 
     #[test]
     fn sample_testcases() {
-        assert_eq!(ChecksumEntry::from("abcdef"), ChecksumEntry { exactly_2_of_any: false, exactly_3_of_any: false });
-        assert_eq!(ChecksumEntry::from("bababc"), ChecksumEntry { exactly_2_of_any: true, exactly_3_of_any: true });
-        assert_eq!(ChecksumEntry::from("abbcde"), ChecksumEntry { exactly_2_of_any: true, exactly_3_of_any: false });
+        assert_eq!(
+            ChecksumEntry::from("abcdef"),
+            ChecksumEntry {
+                exactly_2_of_any: false,
+                exactly_3_of_any: false
+            }
+        );
+        assert_eq!(
+            ChecksumEntry::from("bababc"),
+            ChecksumEntry {
+                exactly_2_of_any: true,
+                exactly_3_of_any: true
+            }
+        );
+        assert_eq!(
+            ChecksumEntry::from("abbcde"),
+            ChecksumEntry {
+                exactly_2_of_any: true,
+                exactly_3_of_any: false
+            }
+        );
     }
 
     #[test]
     fn sample_testcases_2() {
-        let input = ["abcdef", "bababc", "abbcde", "abcccd", "aabcdd", "abcdee", "ababab"];
-        let foo: Vec<ChecksumEntry> = input.iter().map(|&line| ChecksumEntry::from(line)).collect();
+        let input = [
+            "abcdef", "bababc", "abbcde", "abcccd", "aabcdd", "abcdee", "ababab",
+        ];
+        let foo: Vec<ChecksumEntry> = input
+            .iter()
+            .map(|&line| ChecksumEntry::from(line))
+            .collect();
         assert_eq!(checksum(&foo), Checksum(12));
+    }
+
+    #[test]
+    fn run_input() {
+        let entries: Vec<ChecksumEntry> = fs::read_to_string("day2.txt")
+            .unwrap()
+            .lines()
+            .map(ChecksumEntry::from)
+            .collect();
+        assert_eq!(checksum(&entries), Checksum(5928));
     }
 }
