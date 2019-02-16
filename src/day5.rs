00a7f088b9d00031e07e00926b01034ec94f95fc
Dan Fox <dfox@palantir.com>
2019-02-16 21:44:12 +0000

    day5: refactor to allow more brute forcing

--- src/day5.rs
+++ src/day5.rs
@@ -8,11 +8,17 @@ mod test {
     #[test]
     fn part1() {
         let mut bytes: Vec<u8> = fs::read("day5.txt").unwrap();
-        println!("Loaded {} bytes from file", &bytes.len());
+        let squished: Vec<u8> = recursively_collapse(&mut bytes);
 
-        let all_known_bytes: HashSet<u8> = bytes.iter().cloned().collect();
-        assert_eq!(all_known_bytes.contains(&DELETED), false);
+        let x = std::str::from_utf8(&squished).unwrap();
+        dbg!(x);
+        assert_eq!(x.trim().len(), 11194);
+    }
 
+    fn recursively_collapse(entry: &Vec<u8>) -> Vec<u8> {
+        println!("Loaded {} bytes from file", &entry.len());
+
+        let mut bytes: Vec<u8> = entry.clone();
         loop {
             let mut changes = false;
             for i in 0..bytes.len() - 1 {
@@ -33,11 +39,20 @@ mod test {
                     new_vec.push(bytes[i]);
                 }
             }
-            println!("new_vec is {} bytes long", new_vec.len());
             bytes = new_vec;
         }
+        bytes
+    }
 
-        assert_eq!(std::str::from_utf8(bytes.as_ref()).unwrap().trim().len(), 11194);
+    fn all_bytes() -> HashSet<char> {
+        let string = fs::read_to_string("day5.txt").unwrap().to_uppercase();
+        string.chars().collect()
+    }
+
+    #[test]
+    fn check_alphabet_of_polymers() {
+        let all_known_bytes = all_bytes();
+        assert_eq!(all_known_bytes.len(), 27); // there's a newline on the end!
     }
 
     #[inline]
