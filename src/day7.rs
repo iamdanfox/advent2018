9d5a4287bead2086180b15ddfa2840e9804f7798
Dan Fox <dfox@palantir.com>
2019-02-17 12:42:25 +0000

    day7: build forward and backward edge maps

--- src/day7.rs
+++ src/day7.rs
@@ -5,28 +5,44 @@ mod test {
     use std::str::FromStr;
     use std::io::Error;
     use itertools::Itertools;
+    use std::collections::BTreeMap;
 
     #[test]
     fn example() {
-        let sample = r#"Step C must be finished before step A can begin.
-Step C must be finished before step F can begin.
-Step A must be finished before step B can begin.
-Step A must be finished before step D can begin.
-Step B must be finished before step E can begin.
-Step D must be finished before step E can begin.
-Step F must be finished before step E can begin.
-"#;
+        let dependencies = sample_input();
+        let ids = step_ids(&dependencies);
 
-        let input = sample.lines().map(|l| Dependency::from_str(l).unwrap()).collect_vec();
-        dbg!(input);
+        // Step A (key) requires C (values) to be complete
+        let mut back_edges = new_edge_map(&ids);
+        for dep in &dependencies {
+            back_edges.entry(dep.step).or_default().push(dep.prereq);
+        }
+        dbg!(back_edges);
+
+        // Step A (key) is prereq of B, D (values)
+        let mut forward_edges = new_edge_map(&ids);
+        for dep in &dependencies {
+            forward_edges.entry(dep.prereq).or_default().push(dep.step);
+        }
+        dbg!(forward_edges);
+
+    }
+
+    // ensures all node ids appear
+    fn new_edge_map(ids: &[StepId]) -> BTreeMap<StepId, Vec<StepId>> {
+        let mut map: BTreeMap<StepId, Vec<StepId>> = BTreeMap::new();
+        for &id in ids {
+            map.insert(id, Vec::new());
+        }
+        map
     }
 
     type StepId = char;
 
     #[derive(Debug, PartialEq)]
     struct Dependency {
-        step: StepId,
-        depends_on: StepId,
+        pub step: StepId,
+        pub prereq: StepId,
     }
 
     impl FromStr for Dependency {
@@ -38,10 +54,16 @@ Step F must be finished before step E can begin.
 
             let captures = RE.captures(s).expect(&format!("Regex didn't match '{}'", &s));
             Ok(Dependency {
-                depends_on: captures[1].chars().next().expect("Chars for prereq"),
+                prereq: captures[1].chars().next().expect("Chars for prereq"),
                 step: captures[2].chars().next().expect("Chars for step"),
             })
         }
+    }
+
+    fn step_ids(input: &[Dependency]) -> Vec<StepId> {
+        let iter1 = input.iter().map(|d| d.step);
+        let iter2 = input.iter().map(|d| d.prereq);
+        iter1.chain(iter2).sorted().unique().collect()
     }
 
     #[test]
@@ -50,17 +72,29 @@ Step F must be finished before step E can begin.
             Dependency::from_str("Step C must be finished before step A can begin.").unwrap(),
             Dependency {
                 step: 'A',
-                depends_on: 'C'
+                prereq: 'C'
             });
 
-        assert_eq!(parse_input().len(), 101);
+        assert_eq!(real_input().len(), 101);
     }
 
-    fn parse_input() -> Vec<Dependency> {
+    fn real_input() -> Vec<Dependency> {
         fs::read_to_string("day7.txt")
             .unwrap()
             .lines()
             .map(|s| s.parse().unwrap())
             .collect()
+    }
+
+    fn sample_input() -> Vec<Dependency> {
+        let sample = r#"Step C must be finished before step A can begin.
+Step C must be finished before step F can begin.
+Step A must be finished before step B can begin.
+Step A must be finished before step D can begin.
+Step B must be finished before step E can begin.
+Step D must be finished before step E can begin.
+Step F must be finished before step E can begin.
+"#;
+        sample.lines().map(|l| Dependency::from_str(l).unwrap()).collect_vec()
     }
 }
